6d8e936a9e22b31406b5fee09a74db37298c2384
iGlitchOff <iGlitchoff@gmail.com>
2026-09-23 10:00:23 -0500

    Point the phone updater at AutoRewarder-Mobile 4.3.28 (versionCode 40) and package this PC build as 4.3.41.

--- AutoRewarder.iss
+++ AutoRewarder.iss
@@ -1,7 +1,7 @@
 [Setup]
 AppName=AutoRewarder
 AppId=AutoRewarder
-AppVersion=4.3.40
+AppVersion=4.3.41
 AppPublisher=iGlitchOff
 AppPublisherURL=https://github.com/iGlitchOn/AutoRewarder-PC
 DefaultDirName={pf}\AutoRewarder
--- src/apk_update.py
+++ src/apk_update.py
@@ -16,8 +16,8 @@ import urllib.request
 
 from .config import APP_DIR, BASE_DIR
 
-PHONE_VERSION_CODE = 39
-PHONE_VERSION_NAME = "4.3.27"
+PHONE_VERSION_CODE = 40
+PHONE_VERSION_NAME = "4.3.28"
 MOBILE_REPO = "iGlitchOn/AutoRewarder-Mobile"
 
 
